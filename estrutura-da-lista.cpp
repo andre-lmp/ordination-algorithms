f8799102d1dcc3fd1d0d49c36a0f9d56667e785f
andre_luiz_melo <andre.pereira@estudante.ifto.edu.br>
2021-05-20 17:42:39 -0300

    Funções listaCheia e listaVazia adicionadas.

--- estrutura-da-lista.cpp
+++ estrutura-da-lista.cpp
@@ -21,6 +21,8 @@ public:
     ListaDupla();                 // CONSTRUTOR
     void insereInicio(int valor); // INSERE NO INÍCIO
     void insereFinal(int valor);  // INSERE NO FIM
+    bool listaVazia();            // ISEMPTY
+    bool listaCheia();            // ISFULL
     void mostraLista();           // EXIBE A LISTA
     void bubbleSort();            // FAZ A ORDENAÇÃO
 };
@@ -29,7 +31,6 @@ public:
 int main()
 {
     ListaDupla lista1; // INSTÂNCIA DA CLASSE
-    int opcao, valor;
 
     lista1.insereFinal(1);
     lista1.insereFinal(7);
@@ -49,9 +50,9 @@ int main()
     lista1.insereInicio(7);
     lista1.insereInicio(7);
     lista1.insereInicio(7);
-    
+
     lista1.bubbleSort();
-    
+
     lista1.mostraLista();
 
     return 0;
@@ -62,71 +63,120 @@ ListaDupla::ListaDupla() // CONSTRUTOR
     primeiro = NULL;
     ultimo = NULL;
 }
+
+bool ListaDupla::listaVazia() // ISEMPTY
+{
+    return primeiro == NULL;
+}
+
+bool ListaDupla::listaCheia() // ISFULL
+{
+    No *Novo;
+    try
+    {
+        Novo = new No;
+        delete Novo;
+        return false;
+    }
+    catch (bad_alloc exception)
+    {
+        return true;
+    }
+}
+
 void ListaDupla::insereInicio(int valor) // INSERE NO INÍCIO
 {
-    No *Novo = new No;
-    Novo->valor = valor;
-    if (primeiro == NULL)
+    if (listaCheia())
     {
-        primeiro = Novo;
-        ultimo = Novo;
+        return;
     }
     else
     {
-        No *Temp;
-        Temp = primeiro;
-        Novo->proximo = Temp;
-        Temp->anterior = Novo;
-        primeiro = Novo;
+        No *Novo = new No;
+        Novo->valor = valor;
+        if (primeiro == NULL)
+        {
+            primeiro = Novo;
+            ultimo = Novo;
+        }
+        else
+        {
+            No *Temp;
+            Temp = primeiro;
+            Novo->proximo = Temp;
+            Temp->anterior = Novo;
+            primeiro = Novo;
+        }
     }
 }
 void ListaDupla::insereFinal(int valor) // INSERE OS VALORES NO FINAL
 {
-    No *Novo = new No;
-    Novo->valor = valor;
-    if (primeiro == NULL)
+    if (listaCheia())
     {
-        primeiro = Novo;
-        ultimo = Novo;
+        return;
     }
     else
     {
-        No *Temp;
-        Temp = ultimo;
-        Novo->anterior = Temp;
-        Temp->proximo = Novo;
-        ultimo = Novo;
+        No *Novo = new No;
+        Novo->valor = valor;
+        if (primeiro == NULL)
+        {
+            primeiro = Novo;
+            ultimo = Novo;
+        }
+        else
+        {
+            No *Temp;
+            Temp = ultimo;
+            Novo->anterior = Temp;
+            Temp->proximo = Novo;
+            ultimo = Novo;
+        }
     }
 }
 
-void ListaDupla::bubbleSort() // BUBBLESORT
+void ListaDupla::bubbleSort() // ORDENA A LISTA
 {
-    No *ini;
-    No *fim = NULL;
-    No *atual;
-
-    for (ini = primeiro; ini->proximo != NULL; ini = ini->proximo)
+    if (listaVazia())
     {
-        for (atual = primeiro; atual->proximo != fim; atual = atual->proximo)
+        return;
+    }
+    else
+    {
+        No *ini;
+        No *fim = NULL;
+        No *atual;
+
+        for (ini = primeiro; ini->proximo != NULL; ini = ini->proximo)
         {
-            if (atual->valor > atual->proximo->valor)
+            for (atual = primeiro; atual->proximo != fim; atual = atual->proximo)
             {
-                int Temp = atual->valor;
-                atual->valor = atual->proximo->valor;
-                atual->proximo->valor = Temp;
+                if (atual->valor > atual->proximo->valor)
+                {
+                    int Temp = atual->valor;
+                    atual->valor = atual->proximo->valor;
+                    atual->proximo->valor = Temp;
+                }
             }
+            fim = atual;
         }
-        fim = atual;
     }
 }
 
 void ListaDupla::mostraLista() // EXIBE A LISTA
 {
-    No *Temp;
-    Temp = primeiro;
-    while (Temp != NULL)
+    if (listaVazia())
     {
-        cout << Temp->valor << endl;
-        Temp = Temp->proximo;
+        return;
+    }
+    else
+    {
+        No *Temp;
+        Temp = primeiro;
+        while (Temp != NULL)
+        {
+            cout << Temp->valor << endl;
+            Temp = Temp->proximo;
+        }
     }
 }
