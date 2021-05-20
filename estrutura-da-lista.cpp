ada48c073e8c819ced69b4af530a0dbf9f5e8784
andre_luiz_melo <andre.pereira@estudante.ifto.edu.br>
2021-05-20 19:04:18 -0300

    Main atualizado

--- estrutura-da-lista.cpp
+++ estrutura-da-lista.cpp
@@ -21,8 +21,9 @@ public:
     ListaDupla();                 // CONSTRUTOR
     void insereInicio(int valor); // INSERE NO INÍCIO
     void insereFinal(int valor);  // INSERE NO FIM
+    bool listaCheia();            // ISFULL
+    bool listaVazia();            // ISEMPTY
     void mostraLista();           // EXIBE A LISTA
-    void bubbleSort();            // FAZ A ORDENAÇÃO
 };
 
 //MAIN
@@ -48,8 +49,7 @@ int main()
     lista1.insereInicio(7);
     lista1.insereInicio(7);
     lista1.insereInicio(7);
-    
-    lista1.bubbleSort();
+
     lista1.mostraLista();
 
     return 0;
@@ -60,71 +60,93 @@ ListaDupla::ListaDupla() // CONSTRUTOR
     primeiro = NULL;
     ultimo = NULL;
 }
+
+bool ListaDupla::listaVazia() // VERIFICA SE A LISTA ESTÁ VAZIA
+{
+    return primeiro == NULL;
+}
+
+bool ListaDupla::listaCheia() // VERIFICA SE A LISTA ESTÁ CHEIA
+{
+    No *Novo;
+    try
+    {
+        Novo = new No;
+        delete Novo;
+        return false;
+    }
+    catch (bad_alloc exception)
+    {
+        return true;
+    }
+}
+
 void ListaDupla::insereInicio(int valor) // INSERE NO INÍCIO
 {
-    No *Novo = new No;
-    Novo->valor = valor;
-    if (primeiro == NULL)
+    if (listaCheia())
     {
-        primeiro = Novo;
-        ultimo = Novo;
+        return;
     }
     else
     {
-        No *Temp;
-        Temp = primeiro;
-        Novo->proximo = Temp;
-        Temp->anterior = Novo;
-        primeiro = Novo;
-    }
-}
-void ListaDupla::insereFinal(int valor) // INSERE OS VALORES NO FINAL
-{
-    No *Novo = new No;
-    Novo->valor = valor;
-    if (primeiro == NULL)
-    {
-        primeiro = Novo;
-        ultimo = Novo;
-    }
-    else
-    {
-        No *Temp;
-        Temp = ultimo;
-        Novo->anterior = Temp;
-        Temp->proximo = Novo;
-        ultimo = Novo;
-    }
-}
-
-void ListaDupla::bubbleSort() // ORDENA A LISTA
-{
-    No *ini;
-    No *fim = NULL;
-    No *atual;
-
-    for (ini = primeiro; ini->proximo != NULL; ini = ini->proximo)
-    {
-        for (atual = primeiro; atual->proximo != fim; atual = atual->proximo)
+        No *Novo = new No;
+        Novo->valor = valor;
+        if (primeiro == NULL)
         {
-            if (atual->valor > atual->proximo->valor)
-            {
-                int Temp = atual->valor;
-                atual->valor = atual->proximo->valor;
-                atual->proximo->valor = Temp;
-            }
+            primeiro = Novo;
+            ultimo = Novo;
         }
-        fim = atual;
+        else
+        {
+            No *Temp;
+            Temp = primeiro;
+            Novo->proximo = Temp;
+            Temp->anterior = Novo;
+            primeiro = Novo;
+        }
+    }
+}
+
+void ListaDupla::insereFinal(int valor) // INSERE NO FINAL
+{
+    if (listaCheia())
+    {
+        return;
+    }
+    else
+    {
+        No *Novo = new No;
+        Novo->valor = valor;
+        if (primeiro == NULL)
+        {
+            primeiro = Novo;
+            ultimo = Novo;
+        }
+        else
+        {
+            No *Temp;
+            Temp = ultimo;
+            Novo->anterior = Temp;
+            Temp->proximo = Novo;
+            ultimo = Novo;
+        }
     }
 }
 
 void ListaDupla::mostraLista() // EXIBE A LISTA
 {
-    No *Temp;
-    Temp = primeiro;
-    while (Temp != NULL)
+    if (listaVazia())
     {
-        cout << Temp->valor << endl;
-        Temp = Temp->proximo;
+        cout << "ERRO lista vazia!" << endl;
+    }
+    else
+    {
+        No *Temp;
+        Temp = primeiro;
+        while (Temp != NULL)
+        {
+            cout << Temp->valor << endl;
+            Temp = Temp->proximo;
+        }
     }
 }
