98465c60288a0c41eaae3f504c7321cfb44bed8e
andre_luiz_melo <andre.pereira@estudante.ifto.edu.br>
2021-06-30 10:00:33 -0300

    Algoritmo - BubbleSort

--- estrutura-da-lista.cpp
+++ estrutura-da-lista.cpp
@@ -1,3 +1,150 @@
+#include <iostream>
+using namespace std;
+
+// Todo nó da lista tem um valor, um anterior e um próximo.
+struct No
+{
+    int valor;
+    No *anterior = NULL;
+    No *proximo = NULL;
+};
+
+class ListaDupla
+{
+private:
+    //ATRIBUTOS
+    No *primeiro;
+    No *ultimo;
+
+public:
+    //METODOS
+    ListaDupla();                   // CONSTRUTOR
+    void insereInicio(int valor);   // INSERE NO INÍCIO
+    void insereFinal(int valor);    // INSERE NO FIM
+    bool listaVazia();              // ISEMPTY
+    bool listaCheia();              // ISFULL
+    void mostraLista();             // EXIBE A LISTA
+    void trocarValores(No *, No *); // TROCA O VALOR DE 2 NÓS
+    void bubbleSort();              // FAZ A ORDENAÇÃO
+};
+
+//MAIN
+int main()
+{
+    // INSTÂNCIA DA CLASSE
+    ListaDupla lista1;
+
+    lista1.insereFinal(1);
+    lista1.insereFinal(7);
+    lista1.insereFinal(5);
+    lista1.insereFinal(4);
+    lista1.insereInicio(10);
+    lista1.insereInicio(6);
+    lista1.insereInicio(3);
+
+    lista1.bubbleSort();
+    lista1.mostraLista();
+
+    return 0;
+}
+
+// CONSTRUTOR
+ListaDupla::ListaDupla()
+{
+    primeiro = NULL;
+    ultimo = NULL;
+}
+
+// VERIFICA SE A LISTA ESTÁ VAZIA
+bool ListaDupla::listaVazia()
+{
+    return primeiro == NULL;
+}
+
+// VERIFICA SE A LISTA (MEMÓRIA DO COMPUTADOR) ESTÁ CHEIA
+bool ListaDupla::listaCheia()
+{
+    No *Novo;
+    try
+    {
+        Novo = new No;
+        delete Novo;
+        return false;
+    }
+    catch (bad_alloc exception)
+    {
+        return true;
+    }
+}
+
+// INSERE UM VALOR NO INÍCIO DA LISTA
+void ListaDupla::insereInicio(int valor)
+{
+    if (listaCheia())
+    {
+        return;
+    }
+    else
+    {
+        No *Novo = new No;
+        Novo->valor = valor;
+        if (primeiro == NULL)
+        {
+            primeiro = Novo;
+            ultimo = Novo;
+        }
+        else
+        {
+            No *Temp;
+            Temp = primeiro;
+            Novo->proximo = Temp;
+            Temp->anterior = Novo;
+            primeiro = Novo;
+        }
+    }
+}
+
+// INSERE UM VALOR NO FINAL DA LISTA
+void ListaDupla::insereFinal(int valor)
+{
+    if (listaCheia())
+    {
+        return;
+    }
+    else
+    {
+        No *Novo = new No;
+        Novo->valor = valor;
+        if (primeiro == NULL)
+        {
+            primeiro = Novo;
+            ultimo = Novo;
+        }
+        else
+        {
+            No *Temp;
+            Temp = ultimo;
+            Novo->anterior = Temp;
+            Temp->proximo = Novo;
+            ultimo = Novo;
+        }
+    }
+}
+
+// TROCA OS VALORES DE DOIS NÓS
+void ListaDupla::trocarValores(No *v1, No *v2)
+{
+    if (v1->valor == v2->valor)
+    {
+        return;
+    }
+    else
+    {
+        int aux = v1->valor;
+        v1->valor = v2->valor;
+        v2->valor = aux;
+    }
+}
 
 // BUBBLESORT
 void ListaDupla::bubbleSort()
@@ -22,6 +169,25 @@ void ListaDupla::bubbleSort()
                 }
             }
             fim = atual;
+        }
+    }
+}
+
+// EXIBE OS ELEMENTOS DA LISTA
+void ListaDupla::mostraLista()
+{
+    if (listaVazia())
+    {
+        return;
+    }
+    else
+    {
+        No *Temp;
+        Temp = primeiro;
+        while (Temp != NULL)
+        {
+            cout << Temp->valor << endl;
+            Temp = Temp->proximo;
         }
     }
 }
